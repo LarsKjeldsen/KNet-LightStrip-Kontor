fba6ff8591b105e8d844dcefaee8fb120bbe05da
Lars S. Kjeldsen <lars@kjeldsen.net>
2022-03-13 17:32:47 +0100

    Converted to PlatformIO

--- Release/Adafruit_Circuit_Playground/utility/ffft.S.d
+++
@@ -1,2 +0,0 @@
-C:\Users\lars-\AppData\Local\Temp\VMBuilds\LightStrip\esp8266com_generic\Release\Adafruit_Circuit_Playground\utility\ffft.S.o: \
- C:\Users\lars-\Documents\Arduino\libraries\Adafruit_Circuit_Playground\utility\ffft.S
--- Release/FastLED/platforms.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\lars-\AppData\Local\Temp\VMBuilds\LightStrip\esp8266com_generic\Release\FastLED\platforms.cpp.o: \
- C:\Users\lars-\Documents\Arduino\libraries\FastLED\platforms.cpp
